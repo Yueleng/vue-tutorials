d6f6b636fd50d16be62b3d173c6e737745a92de5
Yueleng <wangyueleng@gmail.com>
2022-09-08 11:53:31 -0400

    basic: computed, methods and watch.
    - some documentation about diff between computed, methods and watch.
    - showAnswer and disappear in 5 seconds.
    - use `computed` to show filtered vueBlog, also use method to show filtered vueBlog
    - use watch to listen to data change and apply on side effect.

--- FengHua VUE Tutorial/p03_vue_event/index.js
+++ FengHua VUE Tutorial/p03_vue_event/index.js
@@ -1,12 +1,84 @@
 const app = Vue.createApp({
   data() {
     return {
-      showAnswer: true,
+      showAnswer: false,
+      countDown: 5,
+      timer: null,
+      blogPosts: [
+        "Learn Vue",
+        "Vue from Beginner to Expert",
+        "Learn React",
+        "React from Beginner to Expert",
+      ],
+      blogPosts2: [
+        "Learn Vue",
+        "Vue from Beginner to Expert",
+        "Learn React",
+        "React from Beginner to Expert",
+      ],
+      count: 4,
+      newBlog: "",
     };
   },
   computed: {
     label() {
-      return this.showAnswer ? "Hide" : "Show";
+      return this.showAnswer
+        ? "Hide Answer in " + this.countDown
+        : "Show Answer";
+    },
+    vueBlog() {
+      console.log("computed is called");
+      return this.blogPosts.filter((blog) => blog.includes("Vue"));
+    },
+    counter() {
+      return this.vueBlog.length;
+    },
+  },
+  methods: {
+    toggleAnswer() {
+      this.showAnswer = !this.showAnswer;
+    },
+    getVueBlog(_count) {
+      console.log("method is called");
+      return this.blogPosts
+        .filter((blog) => blog.includes("Vue"))
+        .slice(0, _count);
+    },
+    addAfterTwoSeconds() {
+      setTimeout(() => {
+        this.blogPosts2.push("Advanced Vue");
+      }, 2000);
+    },
+  },
+  watch: {
+    showAnswer(newVal, oldVal) {
+      if (newVal) {
+        this.countDown = 5;
+        if (this.timer) {
+          clearInterval(this.timer);
+          this.timer = null;
+        }
+        this.timer = setInterval(() => {
+          this.countDown -= 1;
+          if (this.countDown === 0) {
+            this.showAnswer = false;
+            clearInterval(this.timer);
+            this.timer = null;
+          }
+        }, 1000);
+      }
+    },
+
+    // blogPosts2: {
+    //   handler(newVal) {
+    //     this.count = newVal.length;
+    //   },
+    //   deep: true,
+    // },
+    newBlog(newVal) {
+      setTimeout(() => {
+        this.blogPosts2.push(newVal);
+      }, 2000);
     },
   },
 });
--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # VUE-Tutorial
  VUE Tutorials
+
+## Difference between `computed`, `methods`, `watch`
+  - `computed` will cache the result, while `methods` won't cache the result.
+  - `computed` will recalculate whenever the dependency `data` changed.
+  - `methods` support `parameter` passed into it. More like a javascript function.
+  - `computed` can be used in HTML, like `data`
+  - `method` can be used for more complex operations or for event handlers or for extract common logics.
+  - `computed` usually won't change the value of data, it is usually considered as a combination/calculation of `data`. While `watch` usually change the data and won't have any return.
+  - for arrays in `watch`, use object instead of function.
+  - in `watch`, we usually do some time comsuming or async logic.
+  - in `computed`, you can also change the value of `data`. But it really is not recommended. we usually don't recommend side effect introduced into the `computed`.
+  - in `watch`, you can use the `method` in order to reduce code.
