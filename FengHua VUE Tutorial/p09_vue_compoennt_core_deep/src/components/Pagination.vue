9c57e2d65c57197cb5d848eac1fc008cf8f4c470
Yueleng <wangyueleng@gmail.com>
2022-09-14 11:36:43 -0400

    component core deep init
    - reactive props

---
+++ FengHua VUE Tutorial/p09_vue_compoennt_core_deep/src/components/Pagination.vue
@@ -0,0 +1,62 @@
+<template>
+  <div>
+    <p>Current Page: {{ currentPage }}</p>
+    <button v-for="n in totalPage" @click="currentPage = n">{{ n }}</button>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ["totalPage", "defaultCurrentPage"],
+  data() {
+    return {
+      currentPage: this.defaultCurrentPage,
+    };
+  },
+};
+</script>
+
+<style scoped>
+input {
+  padding: 8px 14px;
+  border: 1px solid hsl(280deg, 50%, 50%);
+  border-radius: 4px;
+  outline: none;
+  background-color: hsl(280deg, 50%, 30%, 0.2);
+  color: white;
+  margin-top: 1em;
+}
+
+/* button {
+  border: none;
+  background: linear-gradient(
+    90deg,
+    hsl(240deg, 50%, 50%),
+    hsl(280deg, 50%, 50%)
+  );
+  padding: 1em 2em;
+  margin-top: 24px;
+  margin-right: 12px;
+  border-radius: 4px;
+  color: white;
+}
+
+div {
+  border-radius: 4px;
+  border: 1px solid hsl(280deg, 100%, 50%);
+  background-color: hsl(280deg, 40%, 50%, 0.2);
+  padding: 2em;
+  width: 100%;
+  position: relative;
+}
+
+button {
+  border: none;
+  background: none;
+  font-size: 18px;
+  position: absolute;
+  right: 18px;
+  top: 12px;
+  color: white;
+} */
+</style>
