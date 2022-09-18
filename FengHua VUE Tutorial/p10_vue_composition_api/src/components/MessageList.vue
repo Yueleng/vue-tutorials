ff54da6cc8da7a5573ab1fadca8245d1a1207810
Yueleng <wangyueleng@gmail.com>
2022-09-17 23:26:24 -0400

    ref

--- FengHua VUE Tutorial/p10_vue_composition_api/src/components/MessageList.vue
+++ FengHua VUE Tutorial/p10_vue_composition_api/src/components/MessageList.vue
@@ -2,16 +2,20 @@
   <ul>
     <li v-for="msg in messages" :key="msg.id">{{ msg.content }}</li>
   </ul>
+  <button @click="messages = []">Click to delete</button>
 </template>
 <script>
+import { ref } from "vue";
 export default {
   setup() {
-    const messages = [
+    const messages = ref([
       { id: 1, content: "这是一条消息提醒1" },
       { id: 2, content: "这是一条消息提醒2" },
       { id: 3, content: "这是一条消息提醒3" },
       { id: 4, content: "这是一条消息提醒4" },
-    ];
+    ]);
+
+    console.log(messages.value);
 
     return { messages };
   },
@@ -43,5 +47,18 @@ li::before {
   height: 8px;
   background-color: hsl(280deg, 100%, 70%);
   border-radius: 100%;
+}
+
+button {
+  border: none;
+  background: linear-gradient(
+    90deg,
+    hsl(240deg, 50%, 50%),
+    hsl(280deg, 50%, 50%)
+  );
+  padding: 1em 2em;
+  margin-top: 24px;
+  border-radius: 4px;
+  color: white;
 }
 </style>
