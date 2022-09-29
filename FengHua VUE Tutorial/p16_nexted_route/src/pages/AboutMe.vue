b3972eeb5f8025dd324ef5cfe7561a51a410f0dc
Yueleng <wangyueleng@gmail.com>
2022-09-29 17:45:15 -0400

    nested route init

---
+++ FengHua VUE Tutorial/p16_nexted_route/src/pages/AboutMe.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="container">
+    <p>Hi 你好！这是关于我的页面</p>
+    <p>更多详情查看：</p>
+    <nav>
+      <router-link to="/about/work">工作经历</router-link>
+      <router-link to="/about/education">教育经历</router-link>
+    </nav>
+    <router-view></router-view>
+  </div>
+</template>
+<script>
+export default {};
+</script>
+<style scoped>
+.container {
+  width: 100vw;
+  height: 100vh;
+  max-width: 100%;
+  padding: 24px;
+
+  background-color: hsl(280deg, 50%, 20%);
+}
+
+p {
+  margin: 12px 0;
+}
+
+nav {
+  padding-top: 16px;
+}
+</style>
