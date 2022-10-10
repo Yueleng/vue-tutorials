a6f6143215c4fc0d98c614872d91276dc43f1793
Alan <wangyueleng@gmail.com>
2022-10-09 21:57:43 -0400

    vue ts project

--- FengHua VUE Tutorial/ts-vue-shop/src/App.vue
+++ FengHua VUE Tutorial/ts-vue-shop/src/App.vue
@@ -1,31 +1,170 @@
 <script setup lang="ts">
-// This starter template is using Vue 3 <script setup> SFCs
-// Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
-import HelloWorld from './components/HelloWorld.vue'
+import { onMounted, provide, reactive, ref, computed, type Ref } from "vue";
+import ShopIcon from "./components/Icon/ShopIcon.vue";
+import ProductItem from "./components/ProductItem.vue";
+import ActionAndFilters from "./components/ActionAndFilters.vue";
+import { Theme } from "./keys/Theme";
+
+interface Product {
+  id: number;
+  title: string;
+  price: number;
+  inStock: boolean;
+  imageUrl: string;
+}
+
+const products = ref<Product[]>([
+  {
+    id: 1,
+    title: "纯棉T恤",
+    price: 99,
+    inStock: true,
+    imageUrl: "/images/t-shirt1.jpg",
+  },
+  {
+    id: 2,
+    title: "夹克外套",
+    price: 300,
+    inStock: true,
+    imageUrl: "/images/jacket1.jpg",
+  },
+  {
+    id: 3,
+    title: "水洗牛仔裤",
+    price: 128,
+    inStock: false,
+    imageUrl: "/images/jeans1.jpg",
+  },
+  {
+    id: 4,
+    title: "印花T恤",
+    price: 79,
+    inStock: true,
+    imageUrl: "/images/t-shirt2.jpg",
+  },
+]);
+
+// const num = ref(0);
+// const str: Ref<string> = ref("Canada");
+// const bool = ref<boolean>(true);
+
+type SortDirections = "asc" | "desc" | "";
+
+interface SortAndFilter {
+  price: SortDirections;
+  name: SortDirections;
+  inStock: boolean | null;
+}
+
+const sortAndFilter: SortAndFilter = reactive({
+  price: "",
+  name: "",
+  inStock: null,
+});
+
+const productResult = computed(() =>
+  products.value
+    .filter((p) =>
+      sortAndFilter.inStock === null
+        ? true
+        : p.inStock === sortAndFilter.inStock
+    )
+    .sort((a, b) => {
+      if (sortAndFilter.price) {
+        return sortAndFilter.price === "asc"
+          ? a.price - b.price
+          : b.price - a.price;
+      }
+      if (sortAndFilter.name) {
+        return sortAndFilter.name === "asc"
+          ? a.title.localeCompare(b.title)
+          : b.title.localeCompare(a.title);
+      }
+      return 0;
+    })
+);
+
+function handleSortByPrice() {
+  if (sortAndFilter.price === "asc") {
+    sortAndFilter.price = "desc";
+  } else {
+    sortAndFilter.price = "asc";
+  }
+  sortAndFilter.name = "";
+}
+
+function handleSortByName() {
+  if (sortAndFilter.name === "asc") {
+    sortAndFilter.name = "desc";
+  } else {
+    sortAndFilter.name = "asc";
+  }
+  sortAndFilter.price = "";
+}
+
+function handleFilterByStock(inStock: boolean | null) {
+  sortAndFilter.inStock = inStock;
+}
+
+provide(Theme, "dark");
+
+provide("siteTitle", "艾伦的小商店");
+
+const actionAndFiltersRef = ref<InstanceType<typeof ActionAndFilters> | null>(
+  null
+);
+
+onMounted(() => {
+  console.log(actionAndFiltersRef.value);
+});
 </script>
 
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-  <HelloWorld msg="Vite + Vue" />
+  <main>
+    <h1><ShopIcon />艾伦的小商店</h1>
+    <ActionAndFilters
+      @sort-by-price="handleSortByPrice"
+      @sort-by-name="handleSortByName"
+      @filter-by-stock="handleFilterByStock"
+      ref="actionAndFiltersRef"
+    />
+    <div class="productList">
+      <ProductItem
+        v-for="product in productResult"
+        :key="product.id"
+        v-bind="product"
+        class="product"
+      >
+      </ProductItem>
+    </div>
+  </main>
 </template>
 
 <style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
+main {
+  display: grid;
+  /* place-items: center; */
+  align-items: center;
+  justify-items: center;
+  gap: 48px;
+  width: 100vw;
+  padding: 24px;
 }
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
+
+h1 {
+  display: flex;
+  align-items: center;
+  gap: 32px;
 }
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
+
+h1 svg {
+  width: 64px;
+  height: 64px;
+}
+.productList {
+  display: grid;
+  grid-template-columns: 1fr 1fr;
+  gap: 48px;
+  width: 60%;
 }
 </style>
---
+++ FengHua VUE Tutorial/ts-vue-shop/src/components/ActionAndFilters.vue
@@ -0,0 +1,41 @@
+<script setup lang="ts">
+import { onMounted, ref } from "vue";
+
+const emit = defineEmits<{
+  (e: "sortByPrice"): void;
+  (e: "sortByName"): void;
+  (e: "filterByStock", inStock: boolean | null): void;
+}>();
+
+function emitFilterByStock(e: Event) {
+  const value = (e.target as HTMLSelectElement).value;
+  let inStock: boolean | null = null;
+  if (value) {
+    inStock = value === "true";
+  }
+  emit("filterByStock", inStock);
+}
+
+const selectRef = ref<HTMLSelectElement | null>(null);
+
+onMounted(() => {
+  console.log(selectRef.value?.selectedIndex);
+});
+</script>
+<template>
+  <div class="actions">
+    <button @click="emit('sortByPrice')">按价格排序</button>
+    <button @click="emit('sortByName')">按名称排序</button>
+    <select @change="emitFilterByStock" ref="selectRef">
+      <option value="">按库存过滤</option>
+      <option value="true">有货</option>
+      <option value="false">无货</option>
+    </select>
+  </div>
+</template>
+<style scoped>
+.actions {
+  display: flex;
+  gap: 24px;
+}
+</style>
