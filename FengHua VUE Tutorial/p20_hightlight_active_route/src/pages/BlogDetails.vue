796758dc3af9ab0f9bab5f0ac64fba01dc0f5f95
Alan <wangyueleng@gmail.com>
2022-10-16 20:27:37 -0400

    组件里的路由守卫配置

--- FengHua VUE Tutorial/p20_hightlight_active_route/src/pages/BlogDetails.vue
+++ FengHua VUE Tutorial/p20_hightlight_active_route/src/pages/BlogDetails.vue
@@ -1,9 +1,41 @@
 <template>
   <article>
-    <h2>博客详情</h2>
-    详情详情详情详情详情详情详情详情详情详情详情详情详情详情详情
+    <h2>{{ blog.title }}</h2>
+    <p>{{ blog.body }}</p>
   </article>
 </template>
+<script>
+import { getBlogPostById } from "../data/blogPosts";
+export default {
+  data() {
+    return {
+      blog: {},
+    };
+  },
+  beforeRouteLeave() {
+    const result = confirm("真的要离开吗？");
+    if (!result) return false;
+  },
+
+  // 也只在路由变化的时候调用，组件复用的时候不会重新执行
+  beforeRouteEnter(to, from, next) {
+    console.log("组件内部路由守卫", to);
+    console.log("beforeRouteEnter 中 this 没有值，因为组件还没有实例化", this);
+    // next回调函数的参数是组件的实例
+    next((vm) => {
+      setTimeout(() => {
+        vm.blog = getBlogPostById(to.params.postId);
+      }, 1000);
+    });
+  },
+  beforeRouteUpdate(to) {
+    this.blog = {};
+    setTimeout(() => {
+      this.blog = getBlogPostById(to.params.postId);
+    }, 1000);
+  },
+};
+</script>
 <style scoped>
 article {
   padding: 48px;
